4c9872488a2600275d449af43458f99a3cd16114
Ruth Ayele <ruth.ayele@mnsu.edu>
2025-04-09 22:56:38 -0500

    Add files via upload

---
+++ FirstUnique.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static int FirstUniqChar(string s)
+    {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            if (charCount.ContainsKey(c))
+            {
+                charCount[c]++;
+            }
+            else
+            {
+                charCount[c] = 1;
+            }
+        }
+
+        for (int i = 0; i < dict.Count;i++)
+        {
+            if (charCount[s[i]] == 1)
+            {
+                return i; 
+            }
+        }
+        return -1;
+    }
+}
---
+++ MergeStrings.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public string MergeString(string word1, string word2)
+    {
+        string merged = "";
+        int maxLength = Math.Max(word1.Length, word2.Length);
+
+        for (int i = 0; i < maxLength; i++)
+        {
+            if (i < word1.Length)
+            {
+                merged += word1[i];
+            }
+            if (i < word2.Length)
+            {
+                merged += word2[i];
+            }
+        }
+        return merged;
+    }
+}
+
